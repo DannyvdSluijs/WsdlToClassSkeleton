1b6536e0a52338e0f17b2820ee9873a4f02df119
Danny van der Sluijs <dvandersluijs@monitorlinq.com>
2016-05-30 20:36:36 +0200

    Updated skeleton to work with latest version WsdlToClass

--- src/WsdlToClassSkeleton/Commands/ForecastCommand.php
+++ src/WsdlToClassSkeleton/Commands/ForecastCommand.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 5.3
  *
- * @copyright 2015 Danny van der Sluijs <dammy.vandersluijs@icloud.com>
+ * @copyright 2016 Danny van der Sluijs <danny.vandersluijs@icloud.com>
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU-GPL
  * @link      http://dannyvandersluijs.nl
  */
@@ -14,10 +14,10 @@ namespace WsdlToClassSkeleton\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
+/**
+ *
+ */
 class ForecastCommand extends Command {
 
     protected function configure()
@@ -34,18 +34,23 @@ EOT
 );
     }
 
+    /**
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $zip = '95014';
-        $client = new \Weather\Soap\Client();
+        $client = new \WsdlToClassSkeleton\Soap\Client();
 
-        $request = new \Weather\Soap\Request\GetCityWeatherByZIP();
+        $request = new \WsdlToClassSkeleton\Soap\Request\GetCityWeatherByZIP();
         $request->ZIP = $zip;
 
         $response = $client->GetCityWeatherByZIP($request);
         $weather = $response->GetCityWeatherByZIPResult;
 
         $output->writeln(sprintf('Weather for: %s %s %s', $weather->City, $weather->State, $zip));
-        $output->writeln(sprintf('Currently it is %s with a temperature of %s', strtolower($weather->Description), $weather->Temperature));
+        $output->writeln(sprintf('Currently it is %s with a temperature of %s degrees', strtolower($weather->Description), $weather->Temperature));
     }
 }
